6fdd7f3105b47ceeb124b8e94b78babb31dc6b37
Apollo Tang <tang.apollo@gmail.com>
2020-12-26 21:08:40 -0500

    clean the import of the original rtk eg

--- b08-2020/s/02-a-todo-app--before-after-rtk/2-todo-app--after/components/App.js
+++ b08-2020/s/02-a-todo-app--before-after-rtk/2-todo-app--after/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Footer from '../features/filters/Footer'
-import AddTodo from '../features/todos/AddTodo'
-import VisibleTodoList from '../features/todos/VisibleTodoList'
+import Footer from '~/features/filters/Footer'
+import AddTodo from '~/features/todos/AddTodo'
+import VisibleTodoList from '~/features/todos/VisibleTodoList'
 
 const App = () => (
   <div>
-    <div>with rtk</div>
+    <h1>with rtk</h1>
     <AddTodo />
     <VisibleTodoList />
     <Footer />
